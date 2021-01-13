7c564926419370ec82d89b26f021ad9c3710eebb
Dan Rouse <dan@rouse.house>
2021-01-12 19:08:09 -0500

    add test button to settings

--- src/QounterSettingsViewController.cpp
+++ src/QounterSettingsViewController.cpp
@@ -365,6 +365,9 @@ void ShowProgressQounterConfig(QountersMinus::QounterSettingsViewController* sel
         } \
     ));
 
+#include "GlobalNamespace/SimpleLevelStarter.hpp"
+#include "GlobalNamespace/BeatmapLevelSO.hpp"
+#include "GlobalNamespace/GameplayModifiers.hpp"
 void QountersMinus::QounterSettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (!firstActivation || !addedToHierarchy) return;
 
@@ -374,9 +377,25 @@ void QountersMinus::QounterSettingsViewController::DidActivate(bool firstActivat
     buttonsScroller->GetComponent<UnityEngine::UI::VerticalLayoutGroup*>()->set_spacing(1.0f);
 
     auto buttonsScrollerRect = buttonsScroller->get_transform()->get_parent()->get_parent()->get_parent()->GetComponent<UnityEngine::RectTransform*>();
-    buttonsScrollerRect->set_sizeDelta(UnityEngine::Vector2(-112.0f, 0.0f));
-    buttonsScrollerRect->set_anchoredPosition(UnityEngine::Vector2(-52.0f, 0.0f));
+    buttonsScrollerRect->set_sizeDelta(UnityEngine::Vector2(-112.0f, -12.0f));
+    buttonsScrollerRect->set_anchoredPosition(UnityEngine::Vector2(-52.0f, 6.0f));
 
+    auto testButton = QuestUI::BeatSaberUI::CreateUIButton(get_transform(), "Test", "PlayButton", il2cpp_utils::MakeDelegate<UnityEngine::Events::UnityAction*>(
+        classof(UnityEngine::Events::UnityAction*), this, +[](QountersMinus::QounterSettingsViewController* self) {
+            auto simpleLevelStarters = UnityEngine::Resources::FindObjectsOfTypeAll<GlobalNamespace::SimpleLevelStarter*>();
+            for (int i = 0; i < simpleLevelStarters->Length(); i++) {
+                auto starter = simpleLevelStarters->values[i];
+                if (starter->get_gameObject()->get_name()->Contains(il2cpp_utils::createcsstr("PerformanceTestLevelButton"))) {
+                    starter->level->set_name(il2cpp_utils::createcsstr("Qounters- Test"));
+                    starter->gameplayModifiers->demoNoObstacles = true;
+                    starter->StartLevel();
+                    return;
+                }
+            }
+        }
+    ));
+    testButton->GetComponent<UnityEngine::RectTransform*>()->set_anchoredPosition(UnityEngine::Vector2(-52.0f, -27.5f));
+    testButton->GetComponent<UnityEngine::RectTransform*>()->set_sizeDelta(UnityEngine::Vector2(27.0f, 10.0f));
     CreateMenuButton("Main", ShowMainConfig);
     CreateMenuButton("Cut", ShowCutQounterConfig);
     CreateMenuButton("Fail", ShowFailQounterConfig);
