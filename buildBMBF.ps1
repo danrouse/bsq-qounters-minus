b59cf9f668ccad0dea9625ca1985a5303d3242d6
Dan Rouse <dan@rouse.house>
2021-01-13 21:48:13 -0500

    update bmbf config for updated deps

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -3,7 +3,7 @@
 
 if ($?) {
     Compress-Archive -Path "./libs/arm64-v8a/libqounters-minus.so", `
-        "./libs/arm64-v8a/libbeatsaber-hook_0_8_4.so", `
+        "./libs/arm64-v8a/libbeatsaber-hook_1_0_9.so", `
         "./libs/arm64-v8a/libcustom-types.so", `
         "./libs/arm64-v8a/libquestui.so", `
         "./bmbfmod.json", `
