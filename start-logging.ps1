fd0f37297e04e5d7c4e8df1fd58f8d1e9f05c921
Dan Rouse <dan@rouse.house>
2021-01-14 20:00:59 -0500

    0.1.3

--- start-logging.ps1
+++ start-logging.ps1
@@ -1,2 +1,2 @@
 $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-adb logcat -T "$timestamp" main-modloader:W QuestHook[qounters-minus`|v0.1.2]:* AndroidRuntime:E *:S
+adb logcat -T "$timestamp" main-modloader:W QuestHook[qounters-minus`|v0.1.3]:* AndroidRuntime:E *:S
