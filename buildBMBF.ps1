2e24dbc6410ee35975d5c6b30d74666093ce02ae
Dan Rouse <dan@rouse.house>
2021-01-27 17:55:56 -0500

    update bmbfmod

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -4,8 +4,6 @@
 if ($?) {
     Compress-Archive -Path "./libs/arm64-v8a/libqounters-minus.so", `
         "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", `
-        "./libs/arm64-v8a/libcustom-types.so", `
-        "./libs/arm64-v8a/libquestui.so", `
         "./bmbfmod.json", `
         "./cover.png" `
         -DestinationPath "./qounters-minus_v0.2.3.zip" -Update
