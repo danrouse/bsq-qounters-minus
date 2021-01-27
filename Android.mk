7236f8a28332321a44abe0a504b3fb02374cce76
Dan Rouse <dan@rouse.house>
2021-01-27 16:47:43 -0500

    dependency update

--- Android.mk
+++ Android.mk
@@ -20,36 +20,38 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 # Build the modloader shared library
 include $(CLEAR_VARS)
-# Creating prebuilt for dependency: modloader - version: 1.0.4
+LOCAL_MODULE := 
 include $(CLEAR_VARS)
-LOCAL_MODULE := modloader
-LOCAL_EXPORT_C_INCLUDES := extern/modloader
-LOCAL_SRC_FILES := extern/libmodloader.so
-include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.10
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.0.12
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_0_10
+LOCAL_MODULE := beatsaber-hook_1_0_12
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_10.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_12.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.4.1
+# Creating prebuilt for dependency: questui - version: 0.4.3
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
 LOCAL_SRC_FILES := extern/libquestui.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.6.1
+# Creating prebuilt for dependency: codegen - version: 0.6.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_6_1
+LOCAL_MODULE := codegen_0_6_2
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_6_1.so
+LOCAL_SRC_FILES := extern/libcodegen_0_6_2.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.4.3
+# Creating prebuilt for dependency: custom-types - version: 0.4.4
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
+include $(PREBUILT_SHARED_LIBRARY)
+# Creating prebuilt for dependency: modloader - version: 1.0.4
+include $(CLEAR_VARS)
+LOCAL_MODULE := modloader
+LOCAL_EXPORT_C_INCLUDES := extern/modloader
+LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -58,9 +60,9 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_10
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_12
 LOCAL_SHARED_LIBRARIES += questui
-LOCAL_SHARED_LIBRARIES += codegen_0_6_1
+LOCAL_SHARED_LIBRARIES += codegen_0_6_2
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_LDLIBS += -llog
 LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"qounters-minus"' -DVERSION='"0.2.2"' -I'./shared' -I'./extern' -isystem'extern/codegen/include'
