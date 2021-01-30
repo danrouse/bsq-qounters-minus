571a3350e8bf66d4d7057fb8691fad8a78e48edb
Dan Rouse <dan@rouse.house>
2021-01-30 14:08:30 -0500

    1.0.0

--- shared/Qounter.hpp
+++ shared/Qounter.hpp
@@ -16,7 +16,7 @@
 #include "TMPro/TextMeshProUGUI.hpp"
 
 // define version for external consumers
-#define QOUNTERS_MINUS_VERSION "0.2.3"
+#define QOUNTERS_MINUS_VERSION "1.0.0"
 
 namespace QountersMinus {
     enum class QounterPosition {
