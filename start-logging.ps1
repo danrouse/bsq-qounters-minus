43dfaf9e5df5321fd942f94e0bb9ae77f04abed4
Dan Rouse <dan@rouse.house>
2021-02-05 15:23:14 -0500

    more logs

--- start-logging.ps1
+++ start-logging.ps1
@@ -1,2 +1,8 @@
 $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
-adb logcat -T "$timestamp" main-modloader:W QuestHook[qounters-minus`|v1.0.1]:* AndroidRuntime:E *:S
+$bspid = adb shell pidof com.beatgames.beatsaber
+while ($bspid -eq "") {
+    Start-Sleep -ms 100
+    $bspid = adb shell pidof com.beatgames.beatsaber
+}
+adb logcat VrApi:S OVRPlatform:S -T "$timestamp" --pid $bspid
+# adb logcat -T "$timestamp" main-modloader:W QuestHook[qounters-minus`|v1.0.1]:* AndroidRuntime:E *:S
