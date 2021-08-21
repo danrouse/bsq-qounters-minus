d081279661b9ff2881595623dcb92bf891ec2582
Dan Rouse <dan@rouse.house>
2021-08-20 20:08:25 -0400

    scripts update, compile 1.1.0

---
+++ buildQMOD.ps1
@@ -0,0 +1,12 @@
+# Builds a .qmod file for loading with QuestPatcher
+& $PSScriptRoot/build.ps1
+
+$ArchiveName = "qounters-minus_v1.1.0.qmod"
+$TempArchiveName = "qounters-minus_v1.1.0.qmod.zip"
+
+Compress-Archive -Path "./libs/arm64-v8a/libqounters-minus.so", `
+    "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", `
+    "./cover.png", `
+    "./mod.json" `
+    -DestinationPath $TempArchiveName -Force
+Move-Item $TempArchiveName $ArchiveName -Force
